62f5a5f1c5bd5207b0c422128fe38035c31fd298
minaMBGH <AAaa12345>
2016-10-30 15:47:11 +0200

    IlkUygulama

---
+++ JASON_HTML/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace JASON_HTML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVeriGoster = new System.Windows.Forms.Button();
+            this.btnVeriaktar = new System.Windows.Forms.Button();
+            this.lstUrunler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVeriGoster
+            // 
+            this.btnVeriGoster.Location = new System.Drawing.Point(24, 23);
+            this.btnVeriGoster.Name = "btnVeriGoster";
+            this.btnVeriGoster.Size = new System.Drawing.Size(106, 43);
+            this.btnVeriGoster.TabIndex = 0;
+            this.btnVeriGoster.Text = "Veri goster!";
+            this.btnVeriGoster.UseVisualStyleBackColor = true;
+            this.btnVeriGoster.Click += new System.EventHandler(this.btnVeriGoster_Click);
+            // 
+            // btnVeriaktar
+            // 
+            this.btnVeriaktar.Location = new System.Drawing.Point(149, 23);
+            this.btnVeriaktar.Name = "btnVeriaktar";
+            this.btnVeriaktar.Size = new System.Drawing.Size(126, 43);
+            this.btnVeriaktar.TabIndex = 1;
+            this.btnVeriaktar.Text = "Veri Aktar!";
+            this.btnVeriaktar.UseVisualStyleBackColor = true;
+            this.btnVeriaktar.Click += new System.EventHandler(this.btnVeriaktar_Click);
+            // 
+            // lstUrunler
+            // 
+            this.lstUrunler.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstUrunler.FormattingEnabled = true;
+            this.lstUrunler.Location = new System.Drawing.Point(0, 107);
+            this.lstUrunler.Name = "lstUrunler";
+            this.lstUrunler.Size = new System.Drawing.Size(287, 238);
+            this.lstUrunler.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 345);
+            this.Controls.Add(this.lstUrunler);
+            this.Controls.Add(this.btnVeriaktar);
+            this.Controls.Add(this.btnVeriGoster);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVeriGoster;
+        private System.Windows.Forms.Button btnVeriaktar;
+        private System.Windows.Forms.ListBox lstUrunler;
+    }
+}
+
---
+++ JASON_HTML/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace JASON_HTML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Urun> urunlerListesi = new List<Urun>();
+        private void btnVeriGoster_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Urunler.xml");
+            var urunElements = doc.GetElementsByTagName("Urun");
+            
+            foreach (XmlElement siaradakiNod in urunElements)
+            {
+                var siradakiElementOzellikleri = "";
+                //var test = siaradakiNod.Attributes["Id"].Value;
+
+                Urun yeniUrun = new Urun();
+                yeniUrun.Id = Convert.ToInt32(siaradakiNod.Attributes["Id"].Value);
+                yeniUrun.Ad = siaradakiNod.Attributes["Ad"].Value;
+                yeniUrun.Fiyat =string.IsNullOrEmpty(siaradakiNod.Attributes["Fiyat"].Value)?0m : Convert.ToDecimal(siaradakiNod.Attributes["Fiyat"].Value);
+                yeniUrun.KategoriAdi = siaradakiNod.Attributes["KategoriAdi"].Value;
+
+                urunlerListesi.Add(yeniUrun);
+
+
+                //foreach (XmlAttribute siradakiAttr in siaradakiNod.Attributes)
+                //{
+                    
+                    
+                //    siradakiElementOzellikleri += siradakiAttr.Value + " ";
+                //}
+                //lstUrunler.Items.Add(siradakiElementOzellikleri);
+
+            }
+
+            lstUrunler.DataSource = (from u in urunlerListesi
+                                     select new
+                                     {
+                                         id = u.Id,
+                                         AdFiyatKategoriAdi = u.Ad + " " + u.Fiyat + " " + u.KategoriAdi
+                                     }).ToList();
+
+            lstUrunler.DisplayMember = "AdFiyatKategoriAdi";
+        
+            lstUrunler.ValueMember = "id";
+
+            
+        }
+
+        private void btnVeriaktar_Click(object sender, EventArgs e)
+        {
+
+            UrunlerDBEntities ent = new UrunlerDBEntities();
+            ent.Urunler.AddRange(urunlerListesi);
+            MessageBox.Show(ent.SaveChanges() > 0 ? " Aktarıldı!!!!!" : "Sorun var");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Urun
+    {
+
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public decimal Fiyat { get; set; }
+        public string KategoriAdi { get; set; }
+
+
+    }
+
+
+    public class UrunlerDBEntities : DbContext
+    {
+        //Tabloları DbSet<ClassAdi> isimleri ile oluştururuz.
+        //Bu sınıfın constructor'ı içerisinde database ayarlamaları yapabiliyoruz (connectionstring gibi).
+        public DbSet<Urun> Urunler { get; set; } //conteximiz içerisindeki veriler. 
+
+
+        public UrunlerDBEntities()
+        {
+            Database.Connection.ConnectionString = "Server=BASB02;Database=UrunlerDBEntities;User id=sa;Password=1234";
+
+            //Veritabanına bağlanmak için gerekli olan connectionString'i burada ayarlayabiliriz. 
+            //Database.Connection.ConnectionString = "Server=.;Database=EticaretEntities;User id=sa;Password=Password1";
+            //Veritabanı oluşturma stratejilerini burada yazabiliriz. 
+            Database.SetInitializer<UrunlerDBEntities>(new CreateDatabaseIfNotExists<UrunlerDBEntities>()); //Eğer veritabanı yoksa bir tane üret. 
+            Database.SetInitializer<UrunlerDBEntities>(new DropCreateDatabaseIfModelChanges<UrunlerDBEntities>()); //Modelde bir değişiklik olduysa eğer veritabanını drop et ve yeniden oluştur. 
+        }
+
+    }
+
+
+}
