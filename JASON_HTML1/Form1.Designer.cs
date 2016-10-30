0cdc2efc49d58904480e988e9fca3564b1b2b57c
minaMBGH <AAaa12345>
2016-10-30 18:30:42 +0200

    son edit JSON

---
+++ JASON_HTML1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace JASON_HTML1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstKisiler = new System.Windows.Forms.ListBox();
+            this.btnVeriCek = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstKisiler.FormattingEnabled = true;
+            this.lstKisiler.ItemHeight = 18;
+            this.lstKisiler.Location = new System.Drawing.Point(0, 83);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(281, 256);
+            this.lstKisiler.TabIndex = 0;
+            // 
+            // btnVeriCek
+            // 
+            this.btnVeriCek.Location = new System.Drawing.Point(12, 30);
+            this.btnVeriCek.Name = "btnVeriCek";
+            this.btnVeriCek.Size = new System.Drawing.Size(95, 29);
+            this.btnVeriCek.TabIndex = 1;
+            this.btnVeriCek.Text = "Verileri Çek!";
+            this.btnVeriCek.UseVisualStyleBackColor = true;
+            this.btnVeriCek.Click += new System.EventHandler(this.btnVeriCek_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 339);
+            this.Controls.Add(this.btnVeriCek);
+            this.Controls.Add(this.lstKisiler);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstKisiler;
+        private System.Windows.Forms.Button btnVeriCek;
+    }
+}
+
---
+++ JASON_HTML1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JASON_HTML1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // JSON da Entity farameWork gibi References-> Sağ tiklayıp -> Nuget ten indirmemiz gerekir
+        ////http://www.newtonsoft.com/
+        private void btnVeriCek_Click(object sender, EventArgs e)
+        {
+            var okunan=File.ReadAllText("Kisiler.json");
+            List<Kisi> kisiListesi = JsonConvert.DeserializeObject<List<Kisi>>(okunan);
+            lstKisiler.DataSource = kisiListesi.Select(k => new { 
+                AdSoyadYas= k.Ad+  " "+ k.Soyad+ " " + k.Yas,           
+                Id=k.Id
+            
+            }).ToList();
+            lstKisiler.DisplayMember = "AdSoyadYas";
+            lstKisiler.ValueMember = "Id";
+        }
+    }
+
+    public class Kisi
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Yas { get; set; }
+
+
+    }
+}
